d1214f85e7f69508a790c68c50b98d95b9a0db43
Chen <1263057656@qq.com>
2015-04-24 17:22:13 +0800

    瀑布流

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -17,43 +17,6 @@ class IndexController extends CommonController {
         $this->show();
     }
     public function read(){
-    	$id = $_REQUEST['id'];
-		$result = M('Article')->where("id=$id")->find();
-		$pre = M('Article')->where("id<$id AND is_del=0")->order('id desc')->find(); //上一篇
-		$next = M('Article')->where("id>$id AND is_del=0")->order('id asc')->find(); //下一篇
-		$this->assign('pre',$pre);
-		$this->assign('next',$next);
-		$this->assign('result',$result);
     	$this->show();
     }
-	public function tag_article(){
-		$this->assign('js_file',CONTROLLER_NAME.'/js/index');
-		
-		$id = $_REQUEST['tag_id'];
-		
-		$tag_name = M('SystemTag')->where("id=$id")->getField('name');
-		
-		$count = M('Article')->where("tag=$id")->count();
-		//分页
-		$page = new \Think\Page($count,4);
-		$page->setConfig('prev','上一页');
-		$page->setConfig('next','下一页');
-		$show = $page->show();
-		
-		$article_list = M('Article')->where("tag=$id")->order('create_time DESC')->limit($page->firstRow.','.$page->listRows)->select();	
-		$tag_list = M('SystemTag')->where("controller='ArticleTag'")->field('id,name')->select();
-		foreach($tag_list as $k=>$v){
-			$where['tag'] = $v['id'];
-			$where['is_del'] = 0;
-			$count = M('Article')->where($where)->count();
-			$tag_list[$k]['count'] = $count;
-		}
-		
-		$this->assign('tag_list',$tag_list);
-		$this->assign('article_list',$article_list);
-		$this->assign('tag_name',$tag_name);
-		$this->assign('count',$count);
-		$this->assign('page',$show);
-		$this->show();
-	}
 }
